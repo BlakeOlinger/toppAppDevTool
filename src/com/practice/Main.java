967e153c3439f18a7aa7da7431c5ff20c0667b48
Blake Olinger <b.olinger.software.solutions.1101@gmail.com>
2019-06-30 18:38:15 -0400

    -- added basic purpose and usage info to Main

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -1,5 +1,20 @@
 package com.practice;
 
+/*
+This jar is used in the ci/cd of the TOPP App
+
+ASSUMPTIONS - this tool assumes the dev folder
+is installed on the desktop - or some other
+specified location
+
+command line args -
+    [0] - latest version jar to copy
+    [1] - copy location for latest jar
+    [2] - config command
+    [3] - config path
+    [4] - file name for git-add <untrackedFile>
+ */
+
 public class Main {
 
     public static void main(String[] args) {
