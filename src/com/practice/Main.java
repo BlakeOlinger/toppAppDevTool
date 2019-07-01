9356b2a2412ddf8ecad2f493ebffaeb5d4d7c837
Blake Olinger <b.olinger.software.solutions.1101@gmail.com>
2019-06-30 21:22:43 -0400

    -- intermediate

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -15,8 +15,29 @@ command line args -
     [4] - file name for git-add <untrackedFile>
  */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
+        var latestPath = args[0];
+        var copyLocation = args[1];
+        var configFilePath = args[2];
+
+        System.out.println(" Copying File - " + latestPath + " to - " + copyLocation);
+
+        try (var latestFile = new FileInputStream(latestPath);
+        var copy = new FileOutputStream(copyLocation);
+        var configFile = new FileOutputStream(configFilePath)) {
+
+            var commandOne = (int) '0';
+            var commandTwo = (int) '1';
+
+        } catch (IOException ignore) {
+            System.out.println(" ERROR: Could Not Copy File");
+        }
     }
 }
